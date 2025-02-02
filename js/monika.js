826eb44e341ae97bdd3944e3da4bb95314373c78
JackTFB <jacktfbofficial@gmail.com>
2025-02-01 19:07:25 -0500

    Added hover effect when you hover over Monika

---
+++ js/monika.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', '../assets/gifs/monikahappy.gif');
+}
+
+function unhover(element) {
+    element.setAttribute('src', '../assets/gifs/monika.gif');
+}
