cb05301b2ad155fd23b71e83c2a4c5fc5657fc34
Jack TFB <jacktfbofficial@gmail.com>
2025-05-12 14:26:01 -0400

    website v3 layout

--- js/monika.js
+++
@@ -1,7 +0,0 @@
-function hover(element) {
-    element.setAttribute('src', '../assets/gifs/monikahappy.gif');
-}
-
-function unhover(element) {
-    element.setAttribute('src', '../assets/gifs/monika.gif');
-}
